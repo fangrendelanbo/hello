df15e1c516a3255f41a729c06d86bdaf15939d3b
康磊 <1098829426@qq.com>
2022-10-09 18:47:20 +0800

    kl

--- src/caishu.java
+++ src/caishu.java
@@ -3,5 +3,6 @@ public class caishu {
         System.out.println("hello,world");
         System.out.println("hello,world");
         System.out.println("hello,world");
+        System.out.println("hello,world");
     }
 }
