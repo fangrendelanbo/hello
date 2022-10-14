5733895682c81da430b54bf6b9b50a8c5924b402
康磊 <1098829426@qq.com>
2022-10-14 17:53:03 +0800

    kl

--- src/sushu.java
+++ src/sushu.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class sushu {
+    public static void main(String[] args){
+        Scanner in =new Scanner(System.in);
+        int x=in.nextInt();
+        for (int i=2;i<x;i++) {
+            if (x % i == 0) {
+                System.out.println(x + "不是素数,i=" + i);
+            } else {
+                System.out.println(x + "是素数,i=" + i);
+            }
+            break;//break可以单独用在每一个if-else分支中，也可以提取公共部分将其拿出来
+        }
+    }
 }
