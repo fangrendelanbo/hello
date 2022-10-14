02f4fa1d5951e3638b4f476e3e97143de28f0a94
康磊 <1098829426@qq.com>
2022-10-14 17:56:25 +0800

    kl

--- src/sushu.java
+++ src/sushu.java
@@ -6,9 +6,9 @@ public class sushu {
         int x=in.nextInt();
         for (int i=2;i<x;i++) {
             if (x % i == 0) {
-                System.out.println(x + "不是素数,i=" + i);
+                System.out.println(x + "不是素数");
             } else {
-                System.out.println(x + "是素数,i=" + i);
+                System.out.println(x + "是素数");
             }
             break;//break可以单独用在每一个if-else分支中，也可以提取公共部分将其拿出来
         }
