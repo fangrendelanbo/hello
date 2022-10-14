25483cdbeaeffc1ec992bc6fe181586ca49f1c9e
康磊 <1098829426@qq.com>
2022-10-14 17:38:04 +0800

    kl

---
+++ src/forxunhuan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class forxunhuan {//for循环计算阶乘
+    public static void main(String[] args){
+        Scanner in =new Scanner(System.in);
+        System.out.print("输出一个数：");
+        int a=in.nextInt();
+        int i;
+        int b = 1;
+        for (i=1;i<=a;i++){
+            b=b*i;
+        }
+        System.out.println("他的阶乘是："+b);
+    }
+}
