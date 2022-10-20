6a0824a62c4ffc9e7f1d96d5736a8b55a54736ff
康磊 <1098829426@qq.com>
2022-10-20 17:43:31 +0800

    kl

--- src/sushu.java
+++ src/sushu.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
-public class sushu {
+public class sushu {//素数判断
     public static void main(String[] args){
         Scanner in =new Scanner(System.in);
         System.out.print("输入一个数：");
-        int x=in.nextInt();
-        int i;
-        int y=0;
-        for (i = 2; i<x; i++) {
-            if (x % i == 0) {
-                System.out.println(x + "no");
-                y=1;
-                break;
+//        int x=in.nextInt();
+        int  x;
+        for(x=2;x<=100;x++){
+            int i;
+            int y=0;
+            for (i = 2; i<x; i++) {
+                if (x % i == 0) {
+                    System.out.println(x + "no");
+                    y=1;
+                    break;//循环控制
+                }
             }
-        }
-        if (y==0){
-            System.out.println(x + "yes");
+            if (y==0){
+                System.out.println(x + "yes");
+            }
         }
     }
 }
