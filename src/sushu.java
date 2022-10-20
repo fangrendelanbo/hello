da2bdd4d1f1551fc6c9e55a18f13f21e2bbff245
康磊 <1098829426@qq.com>
2022-10-20 18:06:49 +0800

    kl

--- src/sushu.java
+++ src/sushu.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
-public class sushu {//素数判断
+public class sushu {//素数判断，保留①部分表示输入判断素数，保留②部分遍历一个区间输出全部的素数，保留③部分用于输出指定个数个素数
     public static void main(String[] args){
-        Scanner in =new Scanner(System.in);
-        System.out.print("输入一个数：");
-//        int x=in.nextInt();
-        int  x;
-        for(x=2;x<=100;x++){
-            int i;
-            int y=0;
-            for (i = 2; i<x; i++) {
-                if (x % i == 0) {
-                    System.out.println(x + "no");
-                    y=1;
-                    break;//循环控制
+        Scanner in =new Scanner(System.in);//①
+//        System.out.print("输入一个数：");//①
+//        int x=in.nextInt();//输入判断素数①
+        int  x;//②③
+        int jishu=0;//②③
+        for(x=2;x<=500;x++){//遍历一个区间输出全部的素数②，③
+            int i;//①②③
+            int y=0;//①②③
+            for (i = 2; i<x; i++) {//①②③
+                if (x % i == 0) {//①②③
+//                    System.out.println(x + "no");//①
+                    y=1;//①②③
+                    break;//循环控制①②③
                 }
             }
-            if (y==0){
-                System.out.println(x + "yes");
+            if (y==0&&jishu<=50){//①②，’jishu‘计数器，用于输出指定个数个素数（此处输出50个）③
+                System.out.println(x + "yes");//①②③
+                jishu=jishu+1;//②③
             }
         }
     }
